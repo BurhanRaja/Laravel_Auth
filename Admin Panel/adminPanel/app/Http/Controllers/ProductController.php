e5b9f48e18303eb72cf9aafc257059d30fcb46ff
BurhanuddinRaja <mails4burhan@gmail.com>
2022-11-04 10:33:44 +0530

    Chart created

--- Admin Panel/adminPanel/app/Http/Controllers/ProductController.php
+++ Admin Panel/adminPanel/app/Http/Controllers/ProductController.php
@@ -3,28 +3,62 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     // To return all the Products
-    public function show() {
+    public function show()
+    {
         $product = Product::all();
+        $monthDate[] = [];
+        foreach ($product as $key => $value) {
+            $month = $value->created_at->format('M');
+            $monthDate[$key] = $month;
+        }
+
+        // $monthDate;
+
+        $products = Product::select('id', 'created_at')
+            ->get()
+            ->groupBy(function ($date) {
+                return Carbon::parse($date->created_at)->format('m'); // grouping by months
+            });
+
+        $productmcount = [];
+        $productArr = [];
+
+        foreach ($products as $key => $value) {
+            $productmcount[(int)$key] = count($value);
+        }
+
+        $monthAll = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        for ($i = 0; $i < 12; $i++) {
+            if (!empty($productmcount[$i])) {
+                $productArr[$i] = $productmcount[$i];
+            } else {
+                $productArr[$i] = 0;
+            }
+        }
 
         return view('panel.pages.Product')->with([
-            'products'=> $product
-        ]);
+            'products' => $product
+        ])->with(['productArr'=> $productArr]);
     }
 
-    public function create() {
+    public function create()
+    {
         return view('panel.pages.productcrud.createProduct');
     }
 
     // To create a Product
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $validate = $request->validate([
-            'name'=> 'required|min:5',
-            'description'=> 'required|min:10'
+            'name' => 'required|min:5',
+            'description' => 'required|min:10'
         ]);
 
         $poidocument = $request->file('productImg');
@@ -46,7 +80,8 @@ class ProductController extends Controller
         return redirect('/dashboard/products');
     }
 
-    public function edit($id) {
+    public function edit($id)
+    {
         $product = Product::find($id);
         return view('panel.pages.productcrud.editProduct', [
             'product' => $product
@@ -54,10 +89,11 @@ class ProductController extends Controller
     }
 
     // To update a Product's detail
-    public function update(Request $request, Product $product) {
+    public function update(Request $request, Product $product)
+    {
         $validate = $request->validate([
-            'name'=> 'required',
-            'description'=> 'required'
+            'name' => 'required',
+            'description' => 'required'
         ]);
 
         $poidocument = $request->file('productImg');
@@ -80,9 +116,9 @@ class ProductController extends Controller
     }
 
     // To delete a Product
-    public function delete(Product $product) {
+    public function delete(Product $product)
+    {
         $product->delete();
         return redirect('/dashboard/products');
     }
-
 }

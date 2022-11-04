31fe036e5ab834ce5d9a0182613f0ae93d962e5d
Burhan Raja <your_email@abc.example>
2022-11-04 10:33:44 +0530

    Chart created

--- Admin Panel/adminPanel/resources/views/partials/products/chart.blade.php
+++ Admin Panel/adminPanel/resources/views/partials/products/chart.blade.php
@@ -2,15 +2,31 @@
     <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 70%;"></canvas>
   </div>
 
+  {{-- {{$productArr[1]}}
+  {{$productArr[2]}}
+  {{$productArr[3]}}
+  {{$productArr[4]}}
+  {{$productArr[5]}}
+  {{$productArr[6]}}
+  {{$productArr[7]}}
+  {{$productArr[8]}}
+  {{$productArr[9]}}
+  {{$productArr[10]}}
+  {{$productArr[11]}}
+  {{$productArr[12]}} --}}
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
+
+    const data = {!! json_encode($productArr)!!}
+    console.log(data)
     var barChartData = {
-      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [
         {
-          label               : 'Digital Goods',
+          label               : 'No. of Products in Month',
           backgroundColor     : 'rgba(60,141,188,0.9)',
           borderColor         : 'rgba(60,141,188,0.8)',
           pointRadius          : false,
@@ -18,19 +34,19 @@
           pointStrokeColor    : 'rgba(60,141,188,1)',
           pointHighlightFill  : '#fff',
           pointHighlightStroke: 'rgba(60,141,188,1)',
-          data                : [28, 48, 40, 19, 86, 27, 90]
+          data                : data
         },
-        {
-          label               : 'Electronics',
-          backgroundColor     : 'rgba(210, 214, 222, 1)',
-          borderColor         : 'rgba(210, 214, 222, 1)',
-          pointRadius         : false,
-          pointColor          : 'rgba(210, 214, 222, 1)',
-          pointStrokeColor    : '#c1c7d1',
-          pointHighlightFill  : '#fff',
-          pointHighlightStroke: 'rgba(220,220,220,1)',
-          data                : [65, 59, 80, 81, 56, 55, 40]
-        },
+        // {
+        //   label               : 'Electronics',
+        //   backgroundColor     : 'rgba(210, 214, 222, 1)',
+        //   borderColor         : 'rgba(210, 214, 222, 1)',
+        //   pointRadius         : false,
+        //   pointColor          : 'rgba(210, 214, 222, 1)',
+        //   pointStrokeColor    : '#c1c7d1',
+        //   pointHighlightFill  : '#fff',
+        //   pointHighlightStroke: 'rgba(220,220,220,1)',
+        //   data                : [65, 59, 80, 81, 56, 55, 40]
+        // },
       ]
     }
     var barChartCanvas = $('#barChart').get(0).getContext('2d')

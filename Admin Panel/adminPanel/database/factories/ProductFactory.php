dbfb15f9c8bf25c07b19d9b3148faecef1d5bff8
BurhanuddinRaja <mails4burhan@gmail.com>
2022-11-05 12:42:53 +0530

    Got the amount sum using Query

--- Admin Panel/adminPanel/database/factories/ProductFactory.php
+++ Admin Panel/adminPanel/database/factories/ProductFactory.php
@@ -9,7 +9,7 @@ $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'description' => $faker->paragraph(1),
-        'amount'=> $faker->randomDigitNotNull(5, true),
+        'amount'=> $faker->randomNumber(5, true),
         'productImg' => $faker->imageUrl(640, 480, 'animals', true)
     ];
 });

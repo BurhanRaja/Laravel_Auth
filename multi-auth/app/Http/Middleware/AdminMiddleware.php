c480348433f50379580f962d9c754b6b62e43d85
BurhanuddinRaja <mails4burhan@gmail.com>
2022-11-12 10:22:12 +0530

    updates

---
+++ multi-auth/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function redirectTo() {
+        if (!auth('admin')->user()) {
+            return redirect('/admin/login');
+        }
+    }
+}
